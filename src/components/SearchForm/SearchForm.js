66033e3b48579bc42863cdfe3af05b05251ca9c9
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-21 21:03:42 -0400

    worked SearchForm.js

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,2 +1,13 @@
 import React from "react";
 import "./SearchForm.css";
+
+function SearchForm (props) {
+    return (
+        <form className = "search mx-auto">
+            <div className="my-4">
+                <input value={props.search}  onChange={props.handleInputChange} className="form-control" id="searchInput" placeholder="Search" />
+            </div>
+        </form>
+
+    );
+}
