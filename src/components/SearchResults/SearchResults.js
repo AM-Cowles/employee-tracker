370c0d9c9c68874e46960df9b36c14a2ccb55a6e
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-21 20:52:21 -0400

    worked SearchReasults.js

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,3 +1,19 @@
 import React from "react";
 import "./SearchResults.css";
 
+function SearchResults(props) {
+    return (
+
+
+    <table className="table table-striped table-hover">
+    <thead>
+    <tr>
+    <th scope="col">Image</th>
+    <th scope="col" onClick={props.handleSortChange}>Name {props.sortAsc && <span><i class="bi bi-sort-alpha-down"></i></span>} {!props.sortAsc && <span><i class="bi bi-sort-alpha-down-alt"></i></span>} </th>
+    <th scope="col">Phone</th>
+    <th scope="col">Email</th>
+    <th scope="col">DOB</th>
+    </tr>
+</thead>
+<tbody>
+    
