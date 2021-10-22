e4a226cb66cb8c38cb347c35facc200165fcf4e3
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-21 20:32:19 -0400

    worked sort.js

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,1 +1,11 @@
 export const sortItems = (employees, sortAsc) => {
+
+    if(sortAsc)
+    {
+        const sortedArray = employees.sort((a,b) => {
+            if(a.Name.toLowerCase() < b.Name.toLowerCase()) return -1;
+            if(a.Name.toLowerCase() > b.Name.toLowerCase()) return 1;
+            return 0;
+        })
+
+        
