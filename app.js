eff46767fca946edcf674d8e4730942af22ad1c2
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-31 17:37:16 -0400

    worked app.js

--- app.js
+++ app.js
@@ -9,3 +9,47 @@ if (err) throw err;
 console.log(`connected as id ${connection.threadId}`);
 init();
 });
+
+// welcome
+function init() {
+    console.log(`Welcome to the Employee Management System (EMS)`)
+    wwyltd();
+}
+// what would you like to do?
+function wwyltd() {
+    inquirer.prompt([{
+    type: "list",
+    name: "doWhat",
+    message: "What would you like to do?",
+    choices: [
+        {
+        name: "View all Employees",
+        value: "VIEW_EMPLOYEES"
+        },
+        {
+        name: "View all Roles",
+        value: "VIEW_ROLES"
+        },
+        {
+        name: "View all Departments",
+        value: "VIEW_DEPT"
+
+        }, {
+        name: "Add employee",
+        value: "ADD_EMPLOYEE"
+        },
+        {
+        name: "Update Employee",
+        value: "UPDATE_EMP"
+        },
+        {
+        name: "Add Role",
+        value: "ADD_ROLE"
+        },
+        {
+        name: "Add Department",
+        value: "ADD_DEPT"
+        }
+
+    ]
+    
